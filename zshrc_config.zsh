1605ad6fd333b437c2a0ae7aac17dc0c6b1367b4
mlorthiois <matthias.lts@gmail.com>
2020-10-26 16:02:40 +0100

    Add zsh_history

--- zshrc_config.zsh
+++ zshrc_config.zsh
@@ -20,3 +20,11 @@ PS1=$(echo $PS1 | sed 's/(base) //')
 alias count='ls | wc -l'
 alias cat='clear && bat'
 alias untargz='tar -zxvf'
+
+### History Configuration
+HISTSIZE=5000               #How many lines of history to keep in memory
+HISTFILE=~/.zsh_history     #Where to save history to disk
+SAVEHIST=5000               #Number of history entries to save to disk
+setopt    appendhistory     #Append history to the history file (no overwriting)
+setopt    sharehistory      #Share history across terminals
+setopt    incappendhistory  #Immediately append to the history file, not just when a term is killed
