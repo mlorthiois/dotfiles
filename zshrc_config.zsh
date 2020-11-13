1c35491003f445b85691587e46cbeb7779e923a2
mlorthiois <matthias.lts@gmail.com>
2020-11-13 12:41:37 +0100

    Fix git issue ssh status bar Iterm

--- zshrc_config.zsh
+++ zshrc_config.zsh
@@ -28,4 +28,88 @@ SAVEHIST=5000               #Number of history entries to save to disk
 setopt    appendhistory     #Append history to the history file (no overwriting)
 setopt    sharehistory      #Share history across terminals
 setopt    incappendhistory  #Immediately append to the history file, not just when a term is killed
+
 source ~/.iterm2_shell_integration.zsh
+GIT_BINARY=/usr/bin/git
+
+dirty() {
+    # Outputs "dirty" or "clean"
+    OUTPUT=$("$GIT_BINARY" status --porcelain --ignore-submodules -unormal 2>/dev/null)
+    if (($?)); then
+        echo "clean"
+        return
+    fi
+    if [ -z "$OUTPUT" ]; then
+        echo "clean"
+    else
+        echo "dirty"
+    fi
+}
+
+counts() {
+    OUTPUT=$("$GIT_BINARY" rev-list --left-right --count HEAD...@'{u}' 2>/dev/null)
+    if (($?)); then
+        echo "error"
+        return
+    fi
+    echo "$OUTPUT"
+}
+
+branch() {
+    OUTPUT=$("$GIT_BINARY" symbolic-ref -q --short HEAD 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
+    if (($?)); then
+        return
+    fi
+    echo "$OUTPUT"
+}
+
+adds() {
+    "${GIT_BINARY}" ls-files --others --exclude-standard | wc -l
+}
+
+deletes() {
+    "${GIT_BINARY}" ls-files --deleted --exclude-standard | wc -l
+}
+function iterm2_begin_osc {
+  printf "\033]"
+}
+
+function iterm2_end_osc {
+  printf "\007"
+}
+
+function iterm2_set_user_var() {
+  iterm2_begin_osc
+  printf "1337;SetUserVar=%s=%s" "$1" $(printf "%s" "$2" | base64 | tr -d '\n')
+  iterm2_end_osc
+}
+
+git_poll () {
+    DIRECTORY="$1"
+    cd "$DIRECTORY"
+    DIRTY=$(dirty)
+    COUNTS=$(counts)
+    PUSH_COUNT=$(cut -f1 <<< "$COUNTS")
+    PULL_COUNT=$(cut -f2 <<< "$COUNTS")
+    BRANCH=$(branch)
+
+    iterm2_set_user_var gitBranch "$BRANCH"
+    iterm2_set_user_var gitDirty "$DIRTY"
+    iterm2_set_user_var gitPushCount "$PUSH_COUNT"
+    iterm2_set_user_var gitPullCount "$PULL_COUNT"
+    iterm2_set_user_var gitAdds "$ADDS"
+    iterm2_set_user_var gitDeletes "$DELETES"
+}
+
+"$GIT_BINARY" rev-parse --git-dir 2>/dev/null >/dev/null
+if (($?)); then
+    iterm2_set_user_var gitBranch ""
+    iterm2_set_user_var gitDirty ""
+    iterm2_set_user_var gitPushCount ""
+    iterm2_set_user_var gitPullCount ""
+    iterm2_set_user_var gitAdds ""
+    iterm2_set_user_var gitDeletes ""
+else
+    git_poll "$PWD"
+fi
+
