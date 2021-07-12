2b9c2d8ffa0c0069f969a59cb0cbe58f7b87fce7
mlorthiois <matthias.lts@gmail.com>
2021-07-12 11:15:34 +0200

    Add kitty config

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -5,7 +5,7 @@ local on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
         vim.api.nvim_command [[augroup Format]]
         vim.api.nvim_command [[autocmd! * <buffer>]]
-        vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting({}, 1000)]]
+        vim.api.nvim_command [[autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting({}, 1000)]]
         vim.api.nvim_command [[augroup END]]
     end
 end
