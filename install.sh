4b3d3e1c0e2f67ba24fcaabe9c377ce769bc49df
mlorthiois <matthias.lts@gmail.com>
2021-11-10 14:36:49 +0100

    Fast-syntax-highlighting

--- install.sh
+++ install.sh
@@ -5,40 +5,40 @@ dotfiles=$(pwd)
 
 # Symlink of files into ~
 for file in $files; do
-  if [ -e "$HOME/$file" ]; then
-      echo "Symlink of $file to ~"
-      ln -s $dotfiles/$file ~/.$file
-  fi
+	if [ -e "$HOME/$file" ]; then
+		echo "Symlink of $file to ~"
+		ln -s $dotfiles/$file ~/.$file
+	fi
 done
 
 # Symlinks for dir in ~/.config
 config_dirs="nvim kitty"
 for dir in $config_dirs; do
-  if [ ! -d "$HOME/.config/$dir" ]; then
-    echo "Symlink of $dir config to ~/.config/$dir"
-    ln -s $dotfiles/$dir ~/.config
-  fi
+	if [ ! -d "$HOME/.config/$dir" ]; then
+		echo "Symlink of $dir config to ~/.config/$dir"
+		ln -s $dotfiles/$dir ~/.config
+	fi
 done
 
 # Add dependencies for .zshrc
 if [ ! -f "$HOME/.zshrc" ]; then
-  echo "Creating .zshrc file"
-  touch ~/.zshrc
+	echo "Creating .zshrc file"
+	touch ~/.zshrc
 fi
 if ! grep -q "source ~/dotfiles/zsh/zshrc_config.zsh" $HOME/.zshrc; then
-  echo "Add zshrc_config.zsh in .zshrc"
-  echo "source ~/dotfiles/zsh/zshrc_config.zsh" >>$HOME/.zshrc
+	echo "Add zshrc_config.zsh in .zshrc"
+	echo "source ~/dotfiles/zsh/zshrc_config.zsh" >>$HOME/.zshrc
 fi
 
 # Install zsh plugins
-plugins="zsh-syntax-highlighting zsh-autosuggestions"
+plugins="zsh-users/zsh-autosuggestions zdharma-continuum/fast-syntax-highlighting"
 for plugin in $plugins; do
-  if [ ! -d "$HOME/.zsh/$plugin/" ]; then
-    echo "Installing $plugin"
-    mkdir -p $HOME/.zsh
-    cd $HOME/.zsh
-    git clone https://github.com/zsh-users/$plugin.git
-  fi
+	if [ ! -d "$HOME/.zsh/$plugin/" ]; then
+		echo "Installing $plugin"
+		mkdir -p $HOME/.zsh
+		cd $HOME/.zsh
+		git clone https://github.com/$plugin $plugin
+	fi
 done
 cd $dotfiles
 echo "Setup completed!"
--- zsh/zshrc_config.zsh
+++ zsh/zshrc_config.zsh
@@ -17,7 +17,7 @@ export LESS_TERMCAP_md=$'\e[1;34m'
 # Add completions installed through Homebrew packages
 # See: https://docs.brew.sh/Shell-Completion
 if type brew &>/dev/null; then
-  FPATH=/usr/local/share/zsh/site-functions:$FPATH
+	FPATH=/usr/local/share/zsh/site-functions:$FPATH
 fi
 
 # Speed up completion init, see: https://htr3n.github.io/2018/07/faster-zsh/
@@ -38,82 +38,81 @@ setopt AUTO_CD           # Jump without cd
 ##############
 # Set env var based on theme set in kitty conf file
 case $OSTYPE in
-  darwin*)
-    check-kitty-theme() {
-      kitty_config_dir="$HOME/.config/kitty"
-      config_file="$kitty_config_dir/kitty.conf"
-      if grep -q "themes/dark" "$config_file";then  
-        export KITTY_COLORS="dark"
-      else
-        export KITTY_COLORS="light"
-      fi
-    }
-    check-kitty-theme
-    # Switch kitty theme by replacing in kitty.conf
-    kitty-switch-theme() {
-      if [ ! -z $TMUX ]; then
-        echo "Oops... Inside tmux"
-        return 1
-      fi
+darwin*)
+	check-kitty-theme() {
+		kitty_config_dir="$HOME/.config/kitty"
+		config_file="$kitty_config_dir/kitty.conf"
+		if grep -q "themes/dark" "$config_file"; then
+			export KITTY_COLORS="dark"
+		else
+			export KITTY_COLORS="light"
+		fi
+	}
+	check-kitty-theme
+	# Switch kitty theme by replacing in kitty.conf
+	kitty-switch-theme() {
+		if [ ! -z $TMUX ]; then
+			echo "Oops... Inside tmux"
+			return 1
+		fi
 
-      kitty_config_dir="$HOME/.config/kitty"
-      config_file="$kitty_config_dir/kitty.conf"
+		kitty_config_dir="$HOME/.config/kitty"
+		config_file="$kitty_config_dir/kitty.conf"
 
-      if [[ $KITTY_COLORS == "dark" ]];then  
-        echo "Switching to light theme..."
-        sed -i.bak -e 's/themes\/dark.conf/themes\/light.conf/g' $config_file
-        kitty @ set-colors -a "$kitty_config_dir/themes/light.conf"
-        export KITTY_COLORS="light"
-      else
-        echo "Switching to dark theme..."
-        sed -i.bak -e 's/themes\/light.conf/themes\/dark.conf/g' $config_file
-        kitty @ set-colors -a "$kitty_config_dir/themes/dark.conf"
-        export KITTY_COLORS="dark"
-      fi
-      rm $kitty_config_dir/kitty.conf.bak
-    }
-  ;;
+		if [[ $KITTY_COLORS == "dark" ]]; then
+			echo "Switching to light theme..."
+			sed -i.bak -e 's/themes\/dark.conf/themes\/light.conf/g' $config_file
+			kitty @ set-colors -a "$kitty_config_dir/themes/light.conf"
+			export KITTY_COLORS="light"
+		else
+			echo "Switching to dark theme..."
+			sed -i.bak -e 's/themes\/light.conf/themes\/dark.conf/g' $config_file
+			kitty @ set-colors -a "$kitty_config_dir/themes/dark.conf"
+			export KITTY_COLORS="dark"
+		fi
+		rm $kitty_config_dir/kitty.conf.bak
+	}
+	;;
 esac
 
-
 extract() {
-  if [ -f $1 ]; then
-    output=$(echo "${1%.*}")
-    case $1 in
-      *.7z) 7z x $1 ;;
-      *.lzma) unlzma $1 ;;
-      *.rar) unrar x $1 ;;
-      *.tar) tar xvf $1 ;;
-      *.tar.bz2) tar xvjf $1 ;;
-      *.bz2) bunzip2 $1 ;;
-      *.tar.gz) tar xvzf $1 ;;
-      *.gz) gunzip -c $1 >$output ;;
-      *.tar.xz) tar Jxvf $1 ;;
-      *.xz) xz -d $1 ;;
-      *.tbz2) tar xvjf $1 ;;
-      *.tgz) tar xvzf $1 ;;
-      *.zip) unzip $1 ;;
-      *.Z) uncompress ;;
-      *)
-        echo "don't know how to extract $1..."
-        return 1
-        ;;
-    esac
-    echo -e "\e[92m$output successfully created"
-    return 0
-  else
-    echo -e "\e[91mError: the file '$1' doesn't exist!"
-    return 2
-  fi
+	if [ -f $1 ]; then
+		output=$(echo "${1%.*}")
+		case $1 in
+		*.7z) 7z x $1 ;;
+		*.lzma) unlzma $1 ;;
+		*.rar) unrar x $1 ;;
+		*.tar) tar xvf $1 ;;
+		*.tar.bz2) tar xvjf $1 ;;
+		*.bz2) bunzip2 $1 ;;
+		*.tar.gz) tar xvzf $1 ;;
+		*.gz) gunzip -c $1 >$output ;;
+		*.tar.xz) tar Jxvf $1 ;;
+		*.xz) xz -d $1 ;;
+		*.tbz2) tar xvjf $1 ;;
+		*.tgz) tar xvzf $1 ;;
+		*.zip) unzip $1 ;;
+		*.Z) uncompress ;;
+		*)
+			echo "don't know how to extract $1..."
+			return 1
+			;;
+		esac
+		echo -e "\e[92m$output successfully created"
+		return 0
+	else
+		echo -e "\e[91mError: the file '$1' doesn't exist!"
+		return 2
+	fi
 }
 
 ### Use Ctrl-Z to fg - return in NeoVim without adding fg to ZSH History
-function Resume {  
-  fg
-  zle push-input 
-  BUFFER=""
-  zle accept-line
-} 
+function Resume {
+	fg
+	zle push-input
+	BUFFER=""
+	zle accept-line
+}
 zle -N Resume
 bindkey "^Z" Resume
 
@@ -128,29 +127,29 @@ bindkey "^Z" Resume
 # tmux with full dev layout session with FZF (CTRL-F)
 # based on https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/bin/tmux-sessionizer
 tmux-dev() {
-  items=`find ~/Developer -maxdepth 2 -mindepth 2 -type d`
-  items+=("\n$HOME/dotfiles")
-  selected=`echo "$items" | fzf`
+	items=$(find ~/Developer -maxdepth 2 -mindepth 2 -type d)
+	items+=("\n$HOME/dotfiles")
+	selected=$(echo "$items" | fzf)
 
-  # If no project selected, exit function
-  if [ ! $? -eq 0 ]; then
-    return 1
-  fi
+	# If no project selected, exit function
+	if [ ! $? -eq 0 ]; then
+		return 1
+	fi
 
-#   tmux_session_name=`basename $selected | tr . _`
-#   tmux has-session -t="$tmux_session_name" 2> /dev/null
-#   if [ ! $? -eq 0 ]; then
-#     tmux new-session -c $selected -d -s $tmux_session_name
-#     tmux split-window -h -f -p 35 -c $selected
-#     tmux split-window -v -c $selected
-#     tmux select-pane -t 0
-#   fi
-#   tmux attach -t $tmux_session_name
+	#   tmux_session_name=`basename $selected | tr . _`
+	#   tmux has-session -t="$tmux_session_name" 2> /dev/null
+	#   if [ ! $? -eq 0 ]; then
+	#     tmux new-session -c $selected -d -s $tmux_session_name
+	#     tmux split-window -h -f -p 35 -c $selected
+	#     tmux split-window -v -c $selected
+	#     tmux select-pane -t 0
+	#   fi
+	#   tmux attach -t $tmux_session_name
 
-  kitty @ new-window --keep-focus --cwd $selected
-  kitty @ new-window --keep-focus --cwd $selected
-  cd $selected
-  clear
+	kitty @ new-window --keep-focus --cwd $selected
+	kitty @ new-window --keep-focus --cwd $selected
+	cd $selected
+	clear
 }
 bindkey -s '^f' 'tmux-dev^M'
 
@@ -159,20 +158,21 @@ bindkey -s '^f' 'tmux-dev^M'
 ###########
 # Use key-bindings and cli utils
 if [ -e /usr/local/opt/fzf/shell/completion.zsh ]; then
-  source /usr/local/opt/fzf/shell/key-bindings.zsh
-  source /usr/local/opt/fzf/shell/completion.zsh
+	source /usr/local/opt/fzf/shell/key-bindings.zsh
+	source /usr/local/opt/fzf/shell/completion.zsh
 fi
 
 # Use ripgrep
-if type fzf &> /dev/null && type rg &> /dev/null; then
-  export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*" --glob "!vendor/*"'
-  export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
-  export FZF_ALT_C_COMMAND=$FZF_DEFAULT_COMMAND
+if type fzf &>/dev/null && type rg &>/dev/null; then
+	export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*" --glob "!vendor/*"'
+	export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
+	export FZF_ALT_C_COMMAND=$FZF_DEFAULT_COMMAND
 fi
 
 #############
 # LAST MODIFS
 #############
 PS1=$(echo $PS1 | sed 's/(miniconda3) //' | sed 's/(base) //')
-source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOME/.zsh/zsh-users/zsh-autosuggestions/zsh-autosuggestions.zsh
+# source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOME/.zsh/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
