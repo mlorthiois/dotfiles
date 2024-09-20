5e86b35ae23f3f841d7713b539b96199557ab6fb
mlorthiois <matthias.lorthiois@gmail.com>
2024-09-20 15:37:07 +0200

    update

--- zsh/.config/zsh/utils.zsh
+++ zsh/.config/zsh/utils.zsh
@@ -1,3 +1,18 @@
+#########################################################
+# General
+#########################################################
+confirm() {
+  read -q "response?Are you sure? [y/N] "
+  case "$response" in
+    [yY][eE][sS]|[yY]) 
+      true
+      ;;
+    *)
+      exit 0
+      ;;
+  esac
+}
+
 pyright-venv-config() {
   echo '{ "venvPath": ".", "venv": ".venv" }' | jq > pyrightconfig.json
 }
@@ -16,25 +31,21 @@ wd() {
 #########################################################
 # AWS
 #########################################################
+
+# IAM
 select-aws-profile() {
   export AWS_PROFILE=$(aws configure list-profiles | fzf)
 }
 
-export_credentials() {
+export-credentials() {
   eval "$(aws configure export-credentials --format env)"
 }
 
-empty_credentials() {
+empty-credentials() {
   unset AWS_ACCESS_KEY_ID
   unset AWS_SECRET_ACCESS_KEY
   unset AWS_SESSION_TOKEN
   unset AWS_CREDENTIAL_EXPIRATION
-}
-
-empty_model_group() {
-  aws sagemaker list-model-packages --model-package-group-name $1 | \
-    jq -r '.ModelPackageSummaryList[] | .ModelPackageArn' | \
-    xargs -I{} aws sagemaker delete-model-package --model-package-name {}
 }
 
 assume-role() {
@@ -52,3 +63,26 @@ assume-role() {
   echo "Role $role_arn assumed."
 }
 
+# SageMaker
+empty-sm-model-group() {
+  aws sagemaker list-model-packages --model-package-group-name $1 | \
+    jq -r '.ModelPackageSummaryList[] | .ModelPackageArn' | \
+    xargs -I{} aws sagemaker delete-model-package --model-package-name {}
+}
+
+delete-sm-models() {
+  aws sagemaker list-models | \
+    jq -r '.Models.[].ModelName' | \
+    xargs -I{} aws sagemaker delete-model --model-name {}
+}
+
+#########################################################
+# Vault
+#########################################################
+get-vault-token() {
+  export-credentials
+  env=$(echo $AWS_PROFILE | cut -d"-" -f1)
+  echo "Env: $env"
+  get-vault-addr
+  vault login -token-only -method=aws region=eu-west-3
+}
